3cf302ae105e44263b0e1eae5c9fe74fdd4f2e69
Jordan Mason <jordanmason@Jordans-MBP.lan>
2022-05-07 17:48:52 -0400

    db

---
+++ server/index.js
@@ -0,0 +1,21 @@
+
+//IMPORTS
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+//START APP 
+const app = express();
+
+
+ 
+app.use(cors());
+
+
+// CREDS for DB Connection
+const CONNECTION_URL = ""
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect("mongodb+srv://jay2:jay@cluster0.amnj6.mongodb.net/EyeBeauty?retryWrites=true&w=majority", {useNewUrlParser: true})
+    .then(()=> app.listen(PORT, () => console.log('Sever up')))
+    .catch( (error)=> console.log(error.message));
